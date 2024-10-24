8f50ddca3085ad968f5cd665c03aa195c31c4e11
Meghana Merla <24je0654@iitism.ac.in>
2024-10-24 10:21:42 +0530

    Inverse implemented.

---
+++ remote-calculator/calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void inverse(int n){
+	if(n!=0){
+	float x;
+		printf("invers of %d is %f",n,x);
+	}
+	else{
+		printf("no inverse for zero"};
+	}
+int main(void){
+	int n;
+	printf("enter a value:");
+	scanf("%d",&n);
+	inverse(n);
+	}	
